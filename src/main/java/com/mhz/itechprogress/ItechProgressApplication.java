0cfed4325b483a77c3a47a32eb2e6d6b77258c8c
mahdi zgolli <mahdi.zgolli@esprit.tn>
2024-01-28 11:47:57 +0100

    initialize project

---
+++ src/main/java/com/mhz/itechprogress/ItechProgressApplication.java
@@ -0,0 +1,13 @@
+package com.mhz.itechprogress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItechProgressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItechProgressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mhz/itechprogress/ItechProgressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhz.itechprogress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItechProgressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
